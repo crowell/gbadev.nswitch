6d960a2d315b841eda4cf367ec4e1148e0b29b85
beneliezer <beneliezer@gmail.com>
2013-06-19 22:50:40 +0000

    clean up files and maybe commit the changes correctly this time.

---
+++ build/main.d
@@ -0,0 +1,157 @@
+main.o: /home/vwii/gbadev.nswitch/source/main.c \
+ /home/vwii/devkitPro/libogc/include/gccore.h \
+ /home/vwii/devkitPro/libogc/include/ogc/dsp.h \
+ /home/vwii/devkitPro/libogc/include/gctypes.h \
+ /home/vwii/devkitPro/libogc/include/ogc/aram.h \
+ /home/vwii/devkitPro/libogc/include/ogc/arqueue.h \
+ /home/vwii/devkitPro/libogc/include/ogc/lwp_queue.h \
+ /home/vwii/devkitPro/libogc/include/ogc/aram.h \
+ /home/vwii/devkitPro/libogc/include/ogc/arqmgr.h \
+ /home/vwii/devkitPro/libogc/include/ogc/audio.h \
+ /home/vwii/devkitPro/libogc/include/ogc/cache.h \
+ /home/vwii/devkitPro/libogc/include/ogc/card.h \
+ /home/vwii/devkitPro/libogc/include/ogc/cast.h \
+ /home/vwii/devkitPro/libogc/include/ogc/color.h \
+ /home/vwii/devkitPro/libogc/include/ogc/consol.h \
+ /home/vwii/devkitPro/libogc/include/ogc/gx_struct.h \
+ /home/vwii/devkitPro/libogc/include/ogc/dvd.h \
+ /home/vwii/devkitPro/libogc/include/ogc/disc_io.h \
+ /home/vwii/devkitPro/libogc/include/ogc/exi.h \
+ /home/vwii/devkitPro/libogc/include/ogc/gu.h \
+ /home/vwii/devkitPro/libogc/include/ogc/gx.h \
+ /home/vwii/devkitPro/libogc/include/ogc/lwp.h \
+ /home/vwii/devkitPro/libogc/include/ogc/gu.h \
+ /home/vwii/devkitPro/libogc/include/ogc/si.h \
+ /home/vwii/devkitPro/libogc/include/ogc/gx_struct.h \
+ /home/vwii/devkitPro/libogc/include/ogc/irq.h \
+ /home/vwii/devkitPro/libogc/include/ogc/context.h \
+ /home/vwii/devkitPro/libogc/include/ogc/lwp.h \
+ /home/vwii/devkitPro/libogc/include/ogc/mutex.h \
+ /home/vwii/devkitPro/libogc/include/ogc/message.h \
+ /home/vwii/devkitPro/libogc/include/ogc/semaphore.h \
+ /home/vwii/devkitPro/libogc/include/ogc/pad.h \
+ /home/vwii/devkitPro/libogc/include/ogc/tpl.h \
+ /home/vwii/devkitPro/libogc/include/ogc/gx.h \
+ /home/vwii/devkitPro/libogc/include/ogc/system.h \
+ /home/vwii/devkitPro/libogc/include/gcutil.h \
+ /home/vwii/devkitPro/libogc/include/ogc/video.h \
+ /home/vwii/devkitPro/libogc/include/ogc/video_types.h \
+ /home/vwii/devkitPro/libogc/include/ogc/usbgecko.h \
+ /home/vwii/devkitPro/libogc/include/ogc/video_types.h \
+ /home/vwii/devkitPro/libogc/include/ogc/texconv.h \
+ /home/vwii/devkitPro/libogc/include/ogc/ipc.h \
+ /home/vwii/devkitPro/libogc/include/ogc/es.h \
+ /home/vwii/devkitPro/libogc/include/ogc/stm.h \
+ /home/vwii/devkitPro/libogc/include/ogc/ios.h \
+ /home/vwii/devkitPro/libogc/include/ogc/usb.h \
+ /home/vwii/devkitPro/libogc/include/ogc/isfs.h \
+ /home/vwii/devkitPro/libogc/include/ogc/conf.h \
+ /home/vwii/devkitPro/libogc/include/ogc/usbstorage.h \
+ /home/vwii/devkitPro/libogc/include/ogc/mutex.h \
+ /home/vwii/devkitPro/libogc/include/ogc/system.h \
+ /home/vwii/devkitPro/libogc/include/ogc/wiilaunch.h \
+ /home/vwii/gbadev.nswitch/source/iospatch.h
+
+/home/vwii/devkitPro/libogc/include/gccore.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/dsp.h:
+
+/home/vwii/devkitPro/libogc/include/gctypes.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/aram.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/arqueue.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/lwp_queue.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/aram.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/arqmgr.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/audio.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/cache.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/card.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/cast.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/color.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/consol.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/gx_struct.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/dvd.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/disc_io.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/exi.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/gu.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/gx.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/lwp.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/gu.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/si.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/gx_struct.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/irq.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/context.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/lwp.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/mutex.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/message.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/semaphore.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/pad.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/tpl.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/gx.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/system.h:
+
+/home/vwii/devkitPro/libogc/include/gcutil.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/video.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/video_types.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/usbgecko.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/video_types.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/texconv.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/ipc.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/es.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/stm.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/ios.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/usb.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/isfs.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/conf.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/usbstorage.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/mutex.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/system.h:
+
+/home/vwii/devkitPro/libogc/include/ogc/wiilaunch.h:
+
+/home/vwii/gbadev.nswitch/source/iospatch.h:
