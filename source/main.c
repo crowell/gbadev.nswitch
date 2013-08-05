377d782c46c437b4aeaaba4ad660627521752b68
Maxternal <maxternal@gmail.com>
2013-08-04 18:12:05 -0600

    added joostin's edits, fixed the NAND loading and updated the meta.xml

--- source/main.c
+++ source/main.c
@@ -26,9 +26,14 @@
 #include <string.h>
 #include <malloc.h>
 
-#include "iospatch.h"
+#include "runtimeiospatch.h"
 #include "armboot.h"
 
+// Uncomment to enable certain printf functions
+//#DEFINE DEBUG_OUTPUT
+
+#define le32(i) (((((u32) i) & 0xFF) << 24) | ((((u32) i) & 0xFF00) << 8) | \
+				((((u32) i) & 0xFF0000) >> 8) | ((((u32) i) & 0xFF000000) >> 24))
 // Enable interrupt flag.
 #define MSR_EE 0x00008000
  
@@ -45,7 +50,6 @@
  
 // Alignment required for USB structures (I don't know if this is 32 or less).
 #define USB_ALIGN __attribute__ ((aligned(32)))
- 
  
 char bluetoothResetData1[] USB_ALIGN = {0x20}; // bmRequestType
 char bluetoothResetData2[] USB_ALIGN = {0x00}; // bmRequest
@@ -90,9 +94,6 @@ void BTShutdown()
 	IOS_Close(fd);
 }
 
-#define le32(i) (((((u32) i) & 0xFF) << 24) | ((((u32) i) & 0xFF00) << 8) | \
-				((((u32) i) & 0xFF0000) >> 8) | ((((u32) i) & 0xFF000000) >> 24))
-
 typedef struct dol_t dol_t;
 struct dol_t
 {
@@ -108,21 +109,23 @@ struct dol_t
 	u8 pad[0x1C];
 };
 
-int loadDOLfromNAND(const char *path)
+int loadDOLfromNAND(const char *path, bool debug)
 {
-	int fd;
+	int fd ATTRIBUTE_ALIGN(32);
 	s32 fres;
-	dol_t dol_hdr;
+	//fstats *status ATTRIBUTE_ALIGN(32);
+	dol_t dol_hdr ATTRIBUTE_ALIGN(32);
 	
-	printf("Loading DOL file: %s .\n", path);
+	if(debug) printf("Loading DOL file: %s .\n", path);
 	fd = ISFS_Open(path, ISFS_OPEN_READ);
 	if (fd < 0)
 		return fd;
-	printf("Reading header.\n");
+	//printf("ISFS_GetFileStats() returned %d .\n", ISFS_GetFileStats(fd, status));
+	if(debug) printf("Reading header.\n");
 	fres = ISFS_Read(fd, &dol_hdr, sizeof(dol_t));
 	if (fres < 0)
 		return fres;
-	printf("Loading sections.\n");
+	if(debug)printf("Loading sections.\n");
 	int ii;
 
 	/* TEXT SECTIONS */
@@ -136,8 +139,10 @@ int loadDOLfromNAND(const char *path)
 		fres = ISFS_Read(fd, (void*)dol_hdr.addressText[ii], dol_hdr.sizeText[ii]);
 		if (fres < 0)
 			return fres;
-		printf("Text section of size %08x loaded from offset %08x to memory %08x.\n", dol_hdr.sizeText[ii], dol_hdr.offsetText[ii], dol_hdr.addressText[ii]);
-		printf("Memory area starts with %08x and ends with %08x (at address %08x)\n", *(u32*)(dol_hdr.addressData[ii]), *(u32*)((dol_hdr.addressText[ii]+(dol_hdr.sizeText[ii] - 1)) & ~3),(dol_hdr.addressText[ii]+(dol_hdr.sizeText[ii] - 1)) & ~3);
+		if(debug)
+		{	printf("Text section of size %08x loaded from offset %08x to memory %08x.\n", dol_hdr.sizeText[ii], dol_hdr.offsetText[ii], dol_hdr.addressText[ii]);
+			printf("Memory area starts with %08x and ends with %08x (at address %08x)\n", *(u32*)(dol_hdr.addressData[ii]), *(u32*)((dol_hdr.addressText[ii]+(dol_hdr.sizeText[ii] - 1)) & ~3),(dol_hdr.addressText[ii]+(dol_hdr.sizeText[ii] - 1)) & ~3);
+		}
 	}
 
 	/* DATA SECTIONS */
@@ -151,8 +156,10 @@ int loadDOLfromNAND(const char *path)
 		fres = ISFS_Read(fd, (void*)dol_hdr.addressData[ii], dol_hdr.sizeData[ii]);
 		if (fres < 0)
 			return fres;
-		printf("Data section of size %08x loaded from offset %08x to memory %08x.\n", dol_hdr.sizeData[ii], dol_hdr.offsetData[ii], dol_hdr.addressData[ii]);
-		printf("Memory area starts with %08x and ends with %08x (at address %08x)\n", *(u32*)(dol_hdr.addressData[ii]), *(u32*)(dol_hdr.addressData[ii]+(dol_hdr.sizeData[ii] - 1) & ~3),dol_hdr.addressData[ii]+(dol_hdr.sizeData[ii] - 1) & ~3);
+		if(debug)
+		{	printf("Data section of size %08x loaded from offset %08x to memory %08x.\n", dol_hdr.sizeData[ii], dol_hdr.offsetData[ii], dol_hdr.addressData[ii]);
+			printf("Memory area starts with %08x and ends with %08x (at address %08x)\n", *(u32*)(dol_hdr.addressData[ii]), *(u32*)((dol_hdr.addressData[ii]+(dol_hdr.sizeData[ii] - 1)) & ~3),(dol_hdr.addressData[ii]+(dol_hdr.sizeData[ii] - 1)) & ~3);
+		}
 	}
 	
 	ISFS_Close(fd);
@@ -183,24 +190,28 @@ int main() {
 	VIDEO_Init();
 	rmode = VIDEO_GetPreferredMode(NULL);
 	initialize(rmode);
+	
+	#ifdef DEBUG_OUTPUT
 	printf("Applying patches to IOS with AHBPROT\n");
-	printf("IOSPATCH_Apply() returned %d\n", IOSPATCH_Apply());
+	printf("IosPatch_RUNTIME(...) returned %i\n", IosPatch_RUNTIME(true, false, false, true));
 	printf("ISFS_Initialize() returned %d\n", ISFS_Initialize());
-	//printf("__ES_Init() returned %d\n", __ES_Init());
-	//printf("Identify_SysMenu() returned %d\n", Identify_SysMenu());
-	printf("loadDOLfromNAND() returned %d .\n", loadDOLfromNAND("/title/00000001/00000200/content/00000003.app"));
-   
-/*	printf("\nSetting memory.\n");
+	printf("loadDOLfromNAND() returned %d .\n", loadDOLfromNAND("/title/00000001/00000200/content/00000003.app", true);//);
+	printf("Setting magic word.\n");
 	char*redirectedGecko = (char*)0x81200000;
 	*redirectedGecko = (char)(0);
-	printf("Terminating string.\n");
 	*(redirectedGecko+1) = (char)(0);
-	printf("Setting magic word.\n");
 	*((u16*)(redirectedGecko+2)) = 0xDEB6;
-	DCFlushRange(redirectedGecko, 32);*/
-/* 
+	DCFlushRange(redirectedGecko, 32);
+	#else
+	IosPatch_RUNTIME(true, false, false, false);
+	ISFS_Initialize();
+	if(loadDOLfromNAND("/title/00000001/00000200/content/00000003.app", false))
+		printf("Load 1-512 from NAND failed.\n");
+	else printf("1-512 loaded from NAND.\n");
+	#endif
+	 
 	// ** Boot mini from mem code by giantpune ** //
-	void *mini = memalign(32, armboot_size);  
+/*	void *mini = memalign(32, armboot_size);  
 	if(!mini) 
 		  return 0;    
 
@@ -213,10 +224,10 @@ int main() {
 	*(u32*)0xc150f004 = MEM_VIRTUAL_TO_PHYSICAL(mini);  
 	asm volatile("eieio");
 
-	tikview views[4] ATTRIBUTE_ALIGN(32);*/
-	printf("Shutting down IOS subsystems.\n");
+	tikview views[4] ATTRIBUTE_ALIGN(32);
+	//printf("Shutting down IOS subsystems.\n");
 	__IOS_ShutdownSubsystems();
-/*	printf("Loading IOS 254.\n");
+	printf("Loading IOS 254.\n");
 	__ES_Init();
 	u32 numviews;
 	ES_GetNumTicketViews(0x00000001000000FEULL, &numviews);
@@ -225,7 +236,6 @@ int main() {
 
   free(mini);
 */
-
 	// ** boot mini without BootMii IOS code by Crediar ** //
 
 	unsigned char ES_ImportBoot2[16] =
@@ -245,32 +255,37 @@ int main() {
 			*(vu32*)(i+0x08)	= 0xE6000870;	// SYSCALL
 			*(vu32*)(i+0x0C)	= 0xE12FFF1E;	// BLR
 			*(vu32*)(i+0x10)	= 0x10100000;	// offset
-			*(vu32*)(i+0x14)	= 0x0025161F;	// version
+			*(vu32*)(i+0x14)	= 0x0000FF01;	// version
 
 			DCFlushRange( (void*)i, 0x20 );
 
 			void *mini = (void*)0x90100000;
 			memcpy(mini, armboot, armboot_size);
 			DCFlushRange( mini, armboot_size );
-			BTShutdown();
+			
 			s32 fd = IOS_Open( "/dev/es", 0 );
 			
 			u8 *buffer = (u8*)memalign( 32, 0x100 );
 			memset( buffer, 0, 0x100 );
 			
+			#ifdef DEBUG_OUTPUT
 			printf("ES_ImportBoot():%d\n", IOS_IoctlvAsync( fd, 0x1F, 0, 0, (ioctlv*)buffer, NULL, NULL ) );
+			#else
+			IOS_IoctlvAsync( fd, 0x1F, 0, 0, (ioctlv*)buffer, NULL, NULL );
+			#endif
 		}
 	}
-   
-	printf("Waiting for mini to reset PPC\n");
-	while(true);
-/*	{ do DCInvalidateRange(redirectedGecko, 32);
+	#ifdef DEBUG_OUTPUT
+	printf("Waiting for mini gecko output.\n");
+	while(true)
+	{ do DCInvalidateRange(redirectedGecko, 32);
 	  while(!*redirectedGecko);
-	  VIDEO_WaitVSync();
 	  printf(redirectedGecko);
 	  *redirectedGecko = (char)(0);
 	  DCFlushRange(redirectedGecko, 32);
 	}
-*/
+	#else
+	printf("Waiting for ARM to reset PPC.");
+	#endif
 	return 0;
 }
