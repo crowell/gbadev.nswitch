0c30ae6ead8aa744c09853a04589d35ce2dde1ce
Maxternal <maxternal@gmail.com>
2013-10-10 16:09:36 -0600

    Taking out a lot of extra stuff that should no longer be needed on this branch. Also making it decide which ARM code running method to use based on AHBPROT state and making it and made it ALWAYS show Mini gecko output on the screen. Changed the ppcboot.elf path to a nand.bin path as well.

--- source/main.c
+++ source/main.c
@@ -26,7 +26,6 @@
 #include <string.h>
 #include <malloc.h>
 
-#include "runtimeiospatch.h"
 #include "armboot.h"
 
 typedef struct armboot_config armboot_config;
@@ -38,7 +37,7 @@ struct armboot_config
 };
 
 bool __debug = false;
-bool __useIOS = true;
+//bool __useIOS = true;
 armboot_config *redirectedGecko = (armboot_config*)0x81200000;
 
 // Check if string X is in current argument
@@ -54,142 +53,35 @@ armboot_config *redirectedGecko = (armboot_config*)0x81200000;
 // Remeber to set it back to WHITE when you're done
 #define CHANGE_COLOR(X)	(printf((X)))
 
-// Alignment required for USB structures (I don't know if this is 32 or less).
-#define USB_ALIGN __attribute__ ((aligned(32)))
- 
-char bluetoothResetData1[] USB_ALIGN = {0x20}; // bmRequestType
-char bluetoothResetData2[] USB_ALIGN = {0x00}; // bmRequest
-char bluetoothResetData3[] USB_ALIGN = {0x00, 0x00}; // wValue
-char bluetoothResetData4[] USB_ALIGN = {0x00, 0x00}; // wIndex
-char bluetoothResetData5[] USB_ALIGN = {0x03, 0x00}; // wLength
-char bluetoothResetData6[] USB_ALIGN = {0x00}; // unknown; set to zero.
-char bluetoothResetData7[] USB_ALIGN = {0x03, 0x0c, 0x00}; // Mesage payload.
- 
-/** Vectors of data transfered. */
-ioctlv bluetoothReset[] USB_ALIGN = {
-	{	bluetoothResetData1,
-		sizeof(bluetoothResetData1)
-	},{	bluetoothResetData2,
-		sizeof(bluetoothResetData2)
-	},{	bluetoothResetData3,
-		sizeof(bluetoothResetData3)
-	},{	bluetoothResetData4,
-		sizeof(bluetoothResetData4)
-	},{	bluetoothResetData5,
-		sizeof(bluetoothResetData5)
-	},{	bluetoothResetData6,
-		sizeof(bluetoothResetData6)
-	},{	bluetoothResetData7,
-		sizeof(bluetoothResetData7)
-	}
-};
- 
-void BTShutdown()
-{	s32 fd;
-	int rv;
- 
-	printf("Open Bluetooth Dongle\n");
-	fd = IOS_Open("/dev/usb/oh1/57e/305", 2 /* 2 = write, 1 = read */);
-	printf("fd = %d\n", fd);
- 
-	printf("Closing connection to blutooth\n");
-	rv = IOS_Ioctlv(fd, 0, 6, 1, bluetoothReset);
-	printf("ret = %d\n", rv);
- 
-	IOS_Close(fd);
-}
-
 void CheckArguments(int argc, char **argv) {
 	int i;
 	bool pathSet = false;
 	char*newPath = redirectedGecko->buf;
 	if(( pathSet = (argv[0][0] == 's' || argv[0][0] == 'S') )) // Make sure you're using an SD card
 	{	*strrchr(argv[0], '/') = '\0';
-		snprintf(newPath, sizeof(redirectedGecko->buf), "%s/ppcboot.elf", argv[0]+3);
+		snprintf(newPath, sizeof(redirectedGecko->buf), "%s/nand.bin", argv[0]+3);
 	}
 	for (i = 1; i < argc; i++)
 	{	if (CHECK_ARG("debug="))
 			__debug = atoi(CHECK_ARG_VAL("debug="));
 		else if ( pathSet |= (CHECK_ARG("path=")) )
 			strcpy(newPath, CHECK_ARG_VAL("path="));
-		else if (CHECK_ARG("bootmii="))
-			__useIOS = atoi(CHECK_ARG_VAL("bootmii="));
+/*		else if (CHECK_ARG("bootmii="))
+			__useIOS = atoi(CHECK_ARG_VAL("bootmii="));*/
 	}
 	if(pathSet)
 	{	redirectedGecko->path_magic = 0x016AE570;
 		DCFlushRange(redirectedGecko, 288);
-		DEBUG("Setting ppcboot location to %s.\n", newPath);
+		DEBUG("Setting nand.bin location to %s.\n", newPath);
 	}
 }
 
-typedef struct dol_t dol_t;
-struct dol_t
+void disable_memory_protection()
 {
-	u32 offsetText[7];
-	u32 offsetData[11];
-	u32 addressText[7];
-	u32 addressData[11];
-	u32 sizeText[7];
-	u32 sizeData[11];
-	u32 addressBSS;
-	u32 sizeBSS;
-	u32 entrypt;
-	u8 pad[0x1C];
-};
-
-int loadDOLfromNAND(const char *path)
-{
-	int fd ATTRIBUTE_ALIGN(32);
-	s32 fres;
-	dol_t dol_hdr ATTRIBUTE_ALIGN(32);
-	
-	DEBUG("Loading DOL file: %s .\n", path);
-	fd = ISFS_Open(path, ISFS_OPEN_READ);
-	if (fd < 0)
-		return fd;
-	DEBUG("Reading header.\n");
-	fres = ISFS_Read(fd, &dol_hdr, sizeof(dol_t));
-	if (fres < 0)
-		return fres;
-	DEBUG("Loading sections.\n");
-	int ii;
-
-	/* TEXT SECTIONS */
-	for (ii = 0; ii < 7; ii++)
-	{
-		if (!dol_hdr.sizeText[ii])
-			continue;
-		fres = ISFS_Seek(fd, dol_hdr.offsetText[ii], 0);
-		if (fres < 0)
-			return fres;
-		fres = ISFS_Read(fd, (void*)dol_hdr.addressText[ii], dol_hdr.sizeText[ii]);
-		if (fres < 0)
-			return fres;
-		if(__debug)
-		{	printf("Text section of size %08x loaded from offset %08x to memory %08x.\n", dol_hdr.sizeText[ii], dol_hdr.offsetText[ii], dol_hdr.addressText[ii]);
-			printf("Memory area starts with %08x and ends with %08x (at address %08x)\n", *(u32*)(dol_hdr.addressData[ii]), *(u32*)((dol_hdr.addressText[ii]+(dol_hdr.sizeText[ii] - 1)) & ~3),(dol_hdr.addressText[ii]+(dol_hdr.sizeText[ii] - 1)) & ~3);
-		}
-	}
-
-	/* DATA SECTIONS */
-	for (ii = 0; ii < 11; ii++)
-	{
-		if (!dol_hdr.sizeData[ii])
-			continue;
-		fres = ISFS_Seek(fd, dol_hdr.offsetData[ii], 0);
-		if (fres < 0)
-			return fres;
-		fres = ISFS_Read(fd, (void*)dol_hdr.addressData[ii], dol_hdr.sizeData[ii]);
-		if (fres < 0)
-			return fres;
-		if(__debug)
-		{	printf("Data section of size %08x loaded from offset %08x to memory %08x.\n", dol_hdr.sizeData[ii], dol_hdr.offsetData[ii], dol_hdr.addressData[ii]);
-			printf("Memory area starts with %08x and ends with %08x (at address %08x)\n", *(u32*)(dol_hdr.addressData[ii]), *(u32*)((dol_hdr.addressData[ii]+(dol_hdr.sizeData[ii] - 1)) & ~3),(dol_hdr.addressData[ii]+(dol_hdr.sizeData[ii] - 1)) & ~3);
-		}
-	}
-	
-	ISFS_Close(fd);
-	return 0;
+	write16(MEM_PROT, 0);
+	write16(MEM_PROT_START, 0);
+	write16(MEM_PROT_END, 0);
+	delay(10);
 }
 
 static void initialize(GXRModeObj *rmode)
@@ -216,33 +108,17 @@ int main(int argc, char **argv) {
 	VIDEO_Init();
 	rmode = VIDEO_GetPreferredMode(NULL);
 	initialize(rmode);
+	printf("\n\n\n");
 	u32 i;
 	CheckArguments(argc, argv);
-	if(__debug){
-		printf("Applying patches to IOS with AHBPROT\n");
-		printf("IosPatch_RUNTIME(...) returned %i\n", IosPatch_RUNTIME(true, false, false, true));
-		printf("ISFS_Initialize() returned %d\n", ISFS_Initialize());
-		printf("loadDOLfromNAND() returned %d .\n", loadDOLfromNAND("/title/00000001/00000200/content/00000003.app"));
-		printf("Setting magic word.\n");
-		redirectedGecko->str[0] = '\0';
-		redirectedGecko->str[1] = '\0';
-		redirectedGecko->debug_magic = 0xDEB6;
-		DCFlushRange(redirectedGecko, 32);
-	}else{
-		IosPatch_RUNTIME(true, false, false, false);
-		ISFS_Initialize();
-		if(loadDOLfromNAND("/title/00000001/00000200/content/00000003.app"))
-		{	
-			CHANGE_COLOR(RED);
-			printf("Load 1-512 from NAND failed.\n");
-			
-		} else {
-			CHANGE_COLOR(GREEN);
-			printf("1-512 loaded from NAND.\n");
-		}
-		CHANGE_COLOR(WHITE); // Restore default
-	}
-	if(__useIOS){
+	DEBUG("Setting magic word.\n");
+	redirectedGecko->str[0] = '\0';
+	redirectedGecko->str[1] = '\0';
+	redirectedGecko->debug_magic = 0xDEB6;
+	DCFlushRange(redirectedGecko, 32);
+	DEBUG("Shutting down IOS subsystems.\n");
+	__IOS_ShutdownSubsystems();
+	if(!AHBPROT_DISABLED){
 	
 			/** Boot mini from mem code by giantpune. **/
 	
@@ -251,7 +127,7 @@ int main(int argc, char **argv) {
 		void *mini = memalign(32, armboot_size);  
 		if(!mini) 
 			  return 0;    
-
+		
 		memcpy(mini, armboot, armboot_size);  
 		DCFlushRange(mini, armboot_size);               
 
@@ -262,8 +138,6 @@ int main(int argc, char **argv) {
 		asm volatile("eieio");
 
 		tikview views[4] ATTRIBUTE_ALIGN(32);
-		DEBUG("Shutting down IOS subsystems.\n");
-		__IOS_ShutdownSubsystems();
 		printf("Loading IOS 254.\n");
 		__ES_Init();
 		u32 numviews;
@@ -283,7 +157,7 @@ int main(int argc, char **argv) {
 			0x68, 0x4B, 0x2B, 0x06, 0xD1, 0x0C, 0x68, 0x8B, 0x2B, 0x00, 0xD1, 0x09, 0x68, 0xC8, 0x68, 0x42
 		};
 		DEBUG("Searching for ES_ImportBoot2.\n");
-		
+		disable_memory_protection();
 		for( i = 0x939F0000; i < 0x939FE000; i+=2 )
 		{	
 			if( memcmp( (void*)(i), ES_ImportBoot2, sizeof(ES_ImportBoot2) ) == 0 )
@@ -298,8 +172,6 @@ int main(int argc, char **argv) {
 				*(vu32*)(i+0x14)	= 0x0000FF01;	// version
 
 				DCFlushRange( (void*)i, 0x20 );
-				DEBUG("Flushed. Shutting down IOS subsystems.\n");
-				__IOS_ShutdownSubsystems();
 				DEBUG("Copying Mini into place.\n");
 				void *mini = (void*)0x90100000;
 				memcpy(mini, armboot, armboot_size);
@@ -315,24 +187,18 @@ int main(int argc, char **argv) {
 				}else{
 					IOS_IoctlvAsync( fd, 0x1F, 0, 0, (ioctlv*)buffer, NULL, NULL );
 				}
+				break;
 			}
 		}
 	}
-	if(__debug) {
-		printf("Waiting for mini gecko output.\n");
-		char* miniDebug = (char*)redirectedGecko;
-		while(true)
-		{	do
-			{	// Repeat until *miniDebug != 0 ("")
-				DCInvalidateRange(miniDebug, 32);
-			} while(!*miniDebug);
-			
-			printf(miniDebug);
-			*miniDebug = '\0';
-			DCFlushRange(miniDebug, 32);
-		}
-	} else {
-		printf("Waiting for ARM to reset PPC.");
+	DEBUG("Waiting for mini gecko output.\n");
+	char* miniDebug = redirectedGecko->str;
+	while(!miniDebug[1])
+	{	do DCInvalidateRange(miniDebug, 32);
+		while(!*miniDebug);
+		printf(miniDebug);
+		*miniDebug = '\0';
+		DCFlushRange(miniDebug, 32);
 	}
 	return 0;
 }
