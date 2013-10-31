fcf55ae4a1e4eb5fac84dded4a8bea96a4a5130a
Maxternal <maxternal@gmail.com>
2013-10-30 21:39:21 -0600

    using a macro now to avoid so much copy and paste.

--- source/main.c
+++ source/main.c
@@ -47,6 +47,11 @@ static char path[38] = "/title/00000001/00000200/00000003.app";
 
 // Remeber to set it back to WHITE when you're done
 #define CHANGE_COLOR(X)	(printf((X)))
+
+#define PATCH(I,X)	DEBUG( "%08x:%08x -> ", (I), *(vu32*)(I) ); \
+					*(vu32*)(I) = (X); \
+					DEBUG( "%08x\n", *(vu32*)(I) ); \
+					+=4;
 /*
 void CheckArguments(int argc, char **argv) {
 	int i;
@@ -123,113 +128,33 @@ int main(int argc, char **argv) {
 			{	DEBUG("Found. Patching.\n");
 				DCInvalidateRange( (void*)i, 0x80 );
 				
-				DEBUG("%08x %08x -> ", i, *(vu32*)i);
-				*(vu32*)i	= 0x477846C0;	// BX PC, NOP (get out of Thumb mode)
-				DEBUG("%08x\n", *(vu32*)i);
-				i+=4;
-				DEBUG("%08x %08x -> ", i, *(vu32*)i);
-				*(vu32*)i	= 0xE59F005C;	// r0 = &path (PC+92)
-				DEBUG("%08x\n", *(vu32*)i);
-				i+=4;
-				DEBUG("%08x %08x -> ", i, *(vu32*)i);
-				*(vu32*)i	= 0xE6000830;	// boot_PPC(path) (syscall 0x41)
-				DEBUG("%08x\n", *(vu32*)i);
-				i+=4;
-				DEBUG("%08x %08x -> ", i, *(vu32*)i);
-				*(vu32*)i	= 0xE59F0044;	// r0 = ARG1 (0x1330100 ... PC+68)
-				DEBUG("%08x\n", *(vu32*)i);
-				i+=4;
-				DEBUG("%08x %08x -> ", i, *(vu32*)i);
-				*(vu32*)i	= 0xE5902000;	// r2 = read32(0x1330100)
-				DEBUG("%08x\n", *(vu32*)i);
-				i+=4;
-				DEBUG("%08x %08x -> ", i, *(vu32*)i);
-				*(vu32*)i	= 0xE59F003C;	// r0 = ARG1 (PC+60)	<===--- LOOPSTART
-				DEBUG("%08x\n", *(vu32*)i);
-				i+=4;
-				DEBUG("%08x %08x -> ", i, *(vu32*)i);
-				*(vu32*)i	= 0xE3A01020;	// MOV r1 0x20 (ARG2)
-				DEBUG("%08x\n", *(vu32*)i);
-				i+=4;
-				DEBUG("%08x %08x -> ", i, *(vu32*)i);
-				*(vu32*)i	= 0xE60007F0;	// sync_before_read(0x1330100,32) (syscall 0x3F)
-				DEBUG("%08x\n", *(vu32*)i);
-				i+=4;
-				DEBUG("%08x %08x -> ", i, *(vu32*)i);
-				*(vu32*)i	= 0xE59F0030;	// r0 = ARG1 (PC+48)
-				DEBUG("%08x\n", *(vu32*)i);
-				i+=4;
-				DEBUG("%08x %08x -> ", i, *(vu32*)i);
-				*(vu32*)i	= 0xE5903000;	// r3 = read32(0x1330100)
-				DEBUG("%08x\n", *(vu32*)i);
-				i+=4;
-				DEBUG("%08x %08x -> ", i, *(vu32*)i);
-				*(vu32*)i	= 0xE1520003;	// compare r2 r3
-				DEBUG("%08x\n", *(vu32*)i);
-				i+=4;
-				DEBUG("%08x %08x -> ", i, *(vu32*)i);
-				*(vu32*)i	= 0x0AFFFFF8;	// BEQ LOOPSTART		<===---
-				DEBUG("%08x\n", *(vu32*)i);
-				i+=4;
-				DEBUG("%08x %08x -> ", i, *(vu32*)i);
-				*(vu32*)i	= 0xE59F0020;	// r0 = ARG1 (PC+32)
-				DEBUG("%08x\n", *(vu32*)i);
-				i+=4;
-				DEBUG("%08x %08x -> ", i, *(vu32*)i);
-				*(vu32*)i	= 0xE59F1020;	// r1 = PPC1 (PC+32)
-				DEBUG("%08x\n", *(vu32*)i);
-				i+=4;
-				DEBUG("%08x %08x -> ", i, *(vu32*)i);
-				*(vu32*)i	= 0xE5801000;	// write32(r0, r1)
-				DEBUG("%08x\n", *(vu32*)i);
-				i+=4;
-				DEBUG("%08x %08x -> ", i, *(vu32*)i);
-				*(vu32*)i	= 0xE59F101C;	// r1 = PPC2 (PC+28)
-				DEBUG("%08x\n", *(vu32*)i);
-				i+=4;
-				DEBUG("%08x %08x -> ", i, *(vu32*)i);
-				*(vu32*)i	= 0xE5801004;	// write32(r0+4, r1)
-				DEBUG("%08x\n", *(vu32*)i);
-				i+=4;
-				DEBUG("%08x %08x -> ", i, *(vu32*)i);
-				*(vu32*)i	= 0xE59F1018;	// r1 = PPC3 (PC+24)
-				DEBUG("%08x\n", *(vu32*)i);
-				i+=4;
-				DEBUG("%08x %08x -> ", i, *(vu32*)i);
-				*(vu32*)i	= 0xE5801008;	// write32(r0+8, r1)
-				DEBUG("%08x\n", *(vu32*)i);
-				i+=4;
-				DEBUG("%08x %08x -> ", i, *(vu32*)i);
-				*(vu32*)i	= 0xE3A01020;	// MOV r1 0x20 (ARG2)
-				DEBUG("%08x\n", *(vu32*)i);
-				i+=4;
-				DEBUG("%08x %08x -> ", i, *(vu32*)i);
-				*(vu32*)i	= 0xE6000810;	// sync_after_write(0x1330100,32)
-				DEBUG("%08x\n", *(vu32*)i);
-				i+=4;
-				DEBUG("%08x %08x -> ", i, *(vu32*)i);
-				*(vu32*)i	= 0xE12FFF1E;	// BLR
-				DEBUG("%08x\n", *(vu32*)i);
-				i+=4;
-				DEBUG("%08x %08x -> ", i, *(vu32*)i);
-				*(vu32*)i	= 0x01330100;	//				<===--- ARG1
-				DEBUG("%08x\n", *(vu32*)i);
-				i+=4;
-				DEBUG("%08x %08x -> ", i, *(vu32*)i);
-				*(vu32*)i	= 0x38802000;	// li r4, 0x2000<===--- PPC1
-				DEBUG("%08x\n", *(vu32*)i);
-				i+=4;
-				DEBUG("%08x %08x -> ", i, *(vu32*)i);
-				*(vu32*)i	= 0x7c800124;	// mtmsr r4		<===--- PPC2
-				DEBUG("%08x\n", *(vu32*)i);
-				i+=4;
-				DEBUG("%08x %08x -> ", i, *(vu32*)i);
-				*(vu32*)i	= 0x48001802;	// b 0x1800		<===--- PPC3 (assuming init stub is at 0x1800)
-				DEBUG("%08x\n", *(vu32*)i);
-				i+=4;
-				DEBUG("%08x %08x -> ", i, *(vu32*)i);
-				*(vu32*)i	= (vu32)MEM_VIRTUAL_TO_PHYSICAL(&path);
-				DEBUG("%08x\n", *(vu32*)i);
+				PATCH(i, 0x477846C0)   // BX PC, NOP (get out of Thumb mode)
+				PATCH(i, 0xE59F005C)   // r0 = &path (PC+92)
+				PATCH(i, 0xE6000830)   // boot_PPC(path) (syscall 0x41)
+				PATCH(i, 0xE59F0044)   // r0 = ARG1 (0x1330100 ... PC+68)
+				PATCH(i, 0xE5902000)   // r2 = read32(0x1330100)
+				PATCH(i, 0xE59F003C)   // r0 = ARG1 (PC+60)    <===--- LOOPSTART
+				PATCH(i, 0xE3A01020)   // MOV r1 0x20 (ARG2)
+				PATCH(i, 0xE60007F0)   // sync_before_read(0x1330100,32) (syscall 0x3F)
+				PATCH(i, 0xE59F0030)   // r0 = ARG1 (PC+48)
+				PATCH(i, 0xE5903000)   // r3 = read32(0x1330100)
+				PATCH(i, 0xE1520003)   // compare r2 r3
+				PATCH(i, 0x0AFFFFF8)   // BEQ LOOPSTART                <===---
+				PATCH(i, 0xE59F0020)   // r0 = ARG1 (PC+32)
+				PATCH(i, 0xE59F1020)   // r1 = PPC1 (PC+32)
+				PATCH(i, 0xE5801000)   // write32(r0, r1)
+				PATCH(i, 0xE59F101C)   // r1 = PPC2 (PC+28)
+				PATCH(i, 0xE5801004)   // write32(r0+4, r1)
+				PATCH(i, 0xE59F1018)   // r1 = PPC3 (PC+24)
+				PATCH(i, 0xE5801008)   // write32(r0+8, r1)
+				PATCH(i, 0xE3A01020)   // MOV r1 0x20 (ARG2)
+				PATCH(i, 0xE6000810)   // sync_after_write(0x1330100,32)
+				PATCH(i, 0xE12FFF1E)   // BLR
+				PATCH(i, 0x01330100)   //                              <===--- ARG1
+				PATCH(i, 0x38802000)   // li r4, 0x2000<===--- PPC1
+				PATCH(i, 0x7c800124)   // mtmsr r4             <===--- PPC2
+				PATCH(i, 0x48001802)   // b 0x1800             <===--- PPC3 (assuming init stub is at 0x1800)
+				PATCH(i, (vu32)MEM_VIRTUAL_TO_PHYSICAL(&path))
 
 				DCFlushRange( (void*)i, 0x80 );
 				
